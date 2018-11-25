2d6c067caa43fc8b923431f8619830b99c368c3b
Joan Sanchez <jsanchezgarcia13@gmail.com>
2018-11-25 00:19:58 +0000

    Android app

---
+++ FingerprintDialogSample/app/src/main/java/com/androidessence/fingerprintdialogsample/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.androidessence.fingerprintdialogsample
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.hardware.fingerprint.FingerprintManagerCompat
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        val manager = FingerprintManagerCompat.from(this)
+        if (manager.isHardwareDetected && manager.hasEnrolledFingerprints()) {
+            showFingerprintAuth()
+        }
+        background.setOnClickListener { view ->
+            if (manager.isHardwareDetected && manager.hasEnrolledFingerprints()) {
+                showFingerprintAuth()
+            } else {
+                Snackbar.make(view, "Fingerprint authentication is not supported.", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+        fab.setOnClickListener { view ->
+            if (manager.isHardwareDetected && manager.hasEnrolledFingerprints()) {
+                showFingerprintAuth()
+            } else {
+                Snackbar.make(view, "Fingerprint authentication is not supported.", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun showFingerprintAuth() {
+        val dialog = FingerprintDialog.newInstance(
+                "Sign In",
+                "Confirm fingerprint to continue."
+        )
+        dialog.show(supportFragmentManager, FingerprintDialog.FRAGMENT_TAG)
+    }
+}
